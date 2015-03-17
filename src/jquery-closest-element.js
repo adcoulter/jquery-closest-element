5059d7770cb3d210af232fd9491f43f4b1f5f0b4
Adam Coulter <coulterad@gmail.com>
2015-03-17 13:14:51 -0500

    fixed some variable names, added some defaults

--- src/jquery-closest-element.js
+++ src/jquery-closest-element.js
@@ -1,20 +1,25 @@
 (function ($, undefined) {
   //todo: add options (container?)
-  $.closestElement = function (coords, selector) {
+  $.closestElement = function (coords, selector, options) {
+    selector || (selector = 'div');
+    options || (options = {});
+
+    options = $.extend({}, options, { container: 'body'});
+
     var closest;
 
-    $(selector).each(function(i, el) {
+    $(options.container).find(selector).each(function(i, el) {
       var offset = $(el).offset(),
        x = coords.x - offset.left,
        y = coords.y - offset.top,
        dist = Math.sqrt((x*x) + (y*y));
 
-      if(i == 0 || distance < closest.dist) {
+      if(i=== 0 || dist < closest.dist) {
         closest = {  el: el, dist: dist };
       }
     });
 
-    return $(closest.element);
+    return $(closest.el);
   };
 
 })(jQuery);
