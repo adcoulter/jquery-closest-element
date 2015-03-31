e88ad17580186b5efbd74e17b85510be16372561
Adam Coulter <coulterad@gmail.com>
2015-03-31 09:45:56 -0500

    adding xWeight & yWeight so that you can disable or prioritize one over the other

--- src/jquery-closest-element.js
+++ src/jquery-closest-element.js
@@ -3,15 +3,22 @@
   $.closestElement = function (coords, selector, options) {
     //setup defaults
     selector = selector || (selector = 'div');
-    options = $.extend({}, options, { container: 'body'});
+    var defaults = {
+      container: 'body',
+      yWeight: 1,
+      xWeight: 1
+    };
+
+    options = $.extend({}, defaults, options);
 
     var closest = { el: '' };
 
     $(options.container).find(selector).each(function(i, el) {
       var offset = $(el).offset(),
-       x = coords[0] - offset.left,
-       y = coords[1] - offset.top,
-       dist = Math.sqrt((x*x) + (y*y));
+        x = Math.abs(coords[0] - offset.left) * options.xWeight,
+        y = Math.abs(coords[1] - offset.top) * options.yWeight;
+
+      dist = Math.sqrt((x*x) + (y*y));
 
       if(!i || dist < closest.dist) {
         closest = { el: el, dist: dist };
